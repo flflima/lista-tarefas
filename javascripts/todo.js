68af33f41cfbe9625fb7c66baba0a3cb2a4c852c
Felipe Lima <flf.felipe@gmail.com>
2016-04-11 14:47:18 -0300

    Commit inicial.

---
+++ javascripts/todo.js
@@ -0,0 +1,82 @@
+$(function() {
+	var $lastClicked;
+	
+	function onTarefaDeleteClick() {
+		$(this).parent(".tarefa-item").off("click").hide("slow", function() {
+			$(this).remove();
+		});
+	}
+	
+	function onTarefaItemClick() {
+		if(!$(this).is($lastClicked)) {
+			if($lastClicked !== undefined) {
+				savePendingEdition($lastClicked);
+			}
+				
+			$lastClicked = $(this);
+				
+			var text = $lastClicked.children(".tarefa-texto").text();
+			
+			var html = "<input type='text' " +
+						"class='tarefa-edit' value='" + text + "'>";
+						
+			$(this).html(html);
+			
+			$(".tarefa-edit").keydown(onTarefaEditKeydown);
+		}
+	}
+	
+	function onTarefaKeyDown() {
+		if(event.which ===13) {
+			addTarefa($("#tarefa").val());
+			$("#tarefa").val("");
+		}
+	}
+	
+	function onTarefaEditKeydown(event) {
+		if(event.which === 13) {
+			savePendingEdition($lastClicked);
+			$lastClicked = undefined;
+		}
+	}
+	
+	function addTarefa(text) {
+		var $tarefa = $("<div />")
+						.addClass("tarefa-item")
+						.append($("<div />")
+								.addClass("tarefa-texto")
+								.text(text))
+						.append($("<div />")
+								.addClass("tarefa-delete"))
+						.append($("<div />")
+								.addClass("clear"));
+		
+		$("#tarefa-list").append($tarefa);
+		
+		$(".tarefa-delete").click(onTarefaDeleteClick);
+		
+		$(".tarefa-item").click(onTarefaItemClick);
+	}
+	
+	function savePendingEdition($tarefa) {
+		var text = $tarefa.children(".tarefa-edit").val();
+		
+		$tarefa.empty();
+		
+		$tarefa.append("<div class='tarefa-texto'>" + text + "</div>")
+				.append("<div class='tarefa-delete'></div>")
+				.append("<div class='clear'></div>");
+
+
+		$(".tarefa-delete").click(onTarefaDeleteClick);
+
+		$tarefa.click(onTarefaItemClick);
+	}
+	
+	$("#tarefa").keydown(onTarefaKeyDown);
+	
+	$(".tarefa-delete").click(onTarefaDeleteClick);
+	
+	$(".tarefa-item").click(onTarefaItemClick);
+}
+);
